99c3f4e89d7e8497959be995371f3fb5f22edb21
Nic Johnson <iheartredsox@gmail.com>
2014-09-19 15:46:59 -0600

    Try to access user object and rev SP lib

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	console.log(res.locals.user);
+  res.render('index', { title: 'Express', user: res.locals.user });
 });
 
 module.exports = router;
